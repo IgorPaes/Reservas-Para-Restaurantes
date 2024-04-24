2cfab82f6bde22c4f0ad7bba16c9cd14baa643fe
zdkHenrique <henriquesena019@gmail.com>
2024-04-24 00:18:41 -0300

    criando conexão com DB

---
+++ src/main/java/br/com/sistemaReservas/dao/Conexao.java
@@ -0,0 +1,78 @@
+package br.com.sistemaReservas.dao;
+
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+public class Conexao {
+    public static String STATUS = "Não conectado";
+    public static String DRIVER = "com.mysql.cj.jdbc.Driver";
+    public static String SERVER = "localhost";
+    public static String DATABASE = "sistema_reserva";
+    public static String LOGIN = "root";
+    public static String SENHA = "P@$$w0rd";
+    public static String URL = "jdbc:mysql://" + SERVER + ":3306/" + DATABASE;
+    public static Connection CONEXAO;
+    public Conexao() {}
+    public static Connection abrirConexao() throws ClassNotFoundException,SQLException {
+
+
+        if(CONEXAO==null)
+        {
+            try {
+
+                Class.forName(DRIVER);
+                CONEXAO = DriverManager.getConnection(URL, LOGIN, SENHA);
+
+                if (CONEXAO != null) {
+                    STATUS = "Conexão realizada com sucesso!";
+                } else {
+                    STATUS = "Não foi possivel realizar a conexão";
+                }
+
+            } catch (ClassNotFoundException e) {
+
+                throw new ClassNotFoundException("O driver expecificado nao foi encontrado.");
+
+            } catch (SQLException e) {
+
+                //Outra falha de conexão
+                throw new SQLException("Erro ao estabelecer a conexão (Ex: login ou senha errados).");
+            }
+
+        }
+        else
+        {
+            try {
+                if(CONEXAO.isClosed())
+                    CONEXAO = DriverManager.getConnection(URL, LOGIN, SENHA);
+            } catch (SQLException ex) {
+                throw new SQLException("Falha ao fechar a conexão.");
+            }
+        }
+        return CONEXAO;
+    }
+
+    public static String getStatusConexao() {
+        return STATUS;
+    }
+
+    public static boolean fecharConexao() throws SQLException {
+
+        boolean retorno = false;
+
+        try {
+            if(CONEXAO!=null){
+                if(!CONEXAO.isClosed())
+                    CONEXAO.close();
+            }
+
+            STATUS = "Não conectado";
+            retorno = true;
+
+        } catch (SQLException e) {
+            retorno = false;
+        }
+
+        return retorno;
+    }
+}
