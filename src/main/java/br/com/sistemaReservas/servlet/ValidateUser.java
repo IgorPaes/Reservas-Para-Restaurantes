38b918d8217b5b4c946e5cbf8e6b80be7cc7e176
AndreSilva0799 <andrevictor2102@hotmail.com>
2024-05-06 10:38:26 -0300

    Ajustando logica da tela de login

--- src/main/java/br/com/sistemaReservas/servlet/ValidateUser.java
+++ src/main/java/br/com/sistemaReservas/servlet/ValidateUser.java
@@ -19,9 +19,19 @@ public class ValidateUser extends HttpServlet {
         String userSenha = request.getParameter("senha");
 
         CheckUser checkUser = new CheckUser();
-        checkUser.validate(userEmail,userSenha);
-        
-      request.getRequestDispatcher("index.html").forward(request, resp);
+         // Só pra ver se o retorno do select está correto
+        System.out.println("SELECT email, senha FROM Clientes WHERE email = '" + userEmail + "' AND senha = '" + userSenha + "'");
+
+        boolean isValid = checkUser.validate(userEmail,userSenha);
+
+        if(isValid){
+            request.getRequestDispatcher("index.html").forward(request, resp);
+        }else{
+            request.setAttribute("errorMessage","Credenciais invalidas. Tente novamente.");
+            resp.sendRedirect("login.html");
+        }
+
+      //request.getRequestDispatcher("index.html").forward(request, resp);
 
     }
 
