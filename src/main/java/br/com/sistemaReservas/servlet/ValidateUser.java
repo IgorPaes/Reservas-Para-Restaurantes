fb97a6dcb98636ed23d485cf4c3ed53f19eab17f
Lucas-sandrade <Lucas__sandrade@outlook.com>
2024-05-14 09:55:45 -0300

    Limpando Cookies

--- src/main/java/br/com/sistemaReservas/servlet/ValidateUser.java
+++ src/main/java/br/com/sistemaReservas/servlet/ValidateUser.java
@@ -21,14 +21,14 @@ public class ValidateUser extends HttpServlet {
         String userSenha = request.getParameter("senha");
 
         CheckUser checkUser = new CheckUser();
-         // Só pra ver se o retorno do select está correto
+        //Só pra ver se o retorno do select está correto
         //System.out.println("SELECT email, senha FROM Clientes WHERE email = '" + userEmail + "' AND senha = '" + userSenha + "'");
 
         boolean isValid = checkUser.validate(userEmail,userSenha);
         if(isValid){
-            Cookie cookie = new Cookie("user", "true");
-            cookie.setMaxAge(30600);
-            resp.addCookie(cookie);
+           // Cookie emailCookie = new Cookie("email", userEmail);
+           // resp.addCookie(emailCookie);
+            //resp.sendRedirect("index.html");
             request.getRequestDispatcher("index.html").forward(request, resp);
         }else{
             // request.setAttribute("errorMessage","Credenciais invalidas. Tente novamente.");
@@ -42,5 +42,7 @@ public class ValidateUser extends HttpServlet {
 
     }
 
-
 }
+
+
+
