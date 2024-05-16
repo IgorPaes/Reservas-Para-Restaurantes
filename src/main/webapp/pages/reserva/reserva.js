39aed66630214998defaaac0ec229590cc631730
Igor <igor.paes88@gmail.com>
2024-05-16 03:43:56 -0300

    Atualizações na tela de reservas

---
+++ src/main/webapp/pages/reserva/reserva.js
@@ -0,0 +1,20 @@
+function limitarAno(input) {
+    const inputValue = input.value;
+    if(inputValue.length > 10) {
+        input.value = inputValue.slice(0, 10);
+    }
+}
+
+let txtQtdPessoas = document.getElementById('qtdPessoas');
+let QtdPessoas = parseInt(txtQtdPessoas.textContent);
+function menosUm() {
+    if(QtdPessoas > 0) {
+        txtQtdPessoas.textContent = --QtdPessoas;
+    }
+}
+
+function maisUm() {
+    if(QtdPessoas < 15) {
+        txtQtdPessoas.textContent = ++QtdPessoas;
+    }
+}
